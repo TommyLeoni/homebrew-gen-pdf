70a97da452652a3d794467242646f17dac16fd18
TommyLeoni <lot2@bfh.ch>
2022-03-07 17:22:07 +0100

    feat(gen-pdf): update Formula for v1.0.1

--- Formula/gen-pdf.rb
+++ Formula/gen-pdf.rb
@@ -1,9 +1,9 @@
 class GenPdf < Formula
 	desc "Basic way to turn webpages into pdfs"
 	homepage "https://github.com/TommyLeoni/homebrew-gen-pdf"
-	url "https://github.com/TommyLeoni/homebrew-gen-pdf/releases/download/v1.0.0/gen-pdf-macos-x64.tar.gz"
+	url "https://github.com/TommyLeoni/homebrew-gen-pdf/releases/download/v1.0.1/gen-pdf-macos-x64.tar.gz"
 	sha256 "68db8759ff2f92924554774c487b8f48ebac0fa0aa591661ee7d81ada7cc9f27"
-	version "1.0.0"
+	version "1.0.1"
 
 	def install
 		bin.install "gen-pdf"
