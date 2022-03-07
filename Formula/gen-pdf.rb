aaf321f78b2da29ade2c1939e4bda382d5507dc6
TommyLeoni <lot2@bfh.ch>
2022-03-07 17:04:18 +0100

    fix(gen-pdf): fix wrong sha256 sum

--- Formula/gen-pdf.rb
+++ Formula/gen-pdf.rb
@@ -2,7 +2,7 @@ class GenPdf < Formula
 	desc "Basic way to turn webpages into pdfs"
 	homepage "https://github.com/TommyLeoni/homebrew-gen-pdf"
 	url "https://github.com/TommyLeoni/homebrew-gen-pdf/releases/download/v1.0.0/gen-pdf-macos-x64.tar.gz"
-	sha256 "c7acf65686327081e82ba8c64f77600e38a3980aba053b65be5dcc2c041e20fa"
+	sha256 "68db8759ff2f92924554774c487b8f48ebac0fa0aa591661ee7d81ada7cc9f27"
 	version "1.0.0"
 
 	def install
